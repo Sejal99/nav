e5e22237c11b94449f95e990bca45bdd051210e5
sejal99 <sejal8974@gmail.com>
2023-10-29 14:23:27 +0530

    new commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar"
+import Pricing from "./pages/Pricing"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+     
+    </>
+  )
+}
+
+export default App
